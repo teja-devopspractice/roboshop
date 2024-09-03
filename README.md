e830feff1d6663801768c060147be6c2182fc0d4
teja-devopspractice <surlaraviteja60@gmail.com>
2024-09-03 09:29:00 +0530

    Create Frontend Script & config files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# roboshop
+# roboshop-shell
+
+## Improvements
+1. Even if user is running the scripts as normal user we should not run the script, it should say to run as root user.
+2. 
--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,9 @@
 yum install nginx -y
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-
-# some files need to be created
-
 systemctl restart nginx
 systemctl enable nginx
 
