89bf3527499af1de693301463659378f127f6ca2
teja-devopspractice <surlaraviteja60@gmail.com>
2024-09-05 11:14:10 +0530

    catalogue.sh changes

--- catalogue.sh
+++ catalogue.sh
@@ -1,6 +1,7 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
+rm -rf /app
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
