9f866cee29ea0f98166b3b806b205cebc8a1d8b5
teja-devopspractice <surlaraviteja60@gmail.com>
2024-09-05 15:31:26 +0530

    cart update

--- cart.sh
+++ cart.sh
@@ -1,12 +1,29 @@
+echo -e "\e[36m>>>>>>>>>> configuring NodeJS repos <<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[36m>>>>>>>>>> install NodeJS <<<<<<<<\e[0m"
 yum install nodejs -y
+
+echo -e "\e[36m>>>>>>>>>> Add Application User <<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>>>> Create Application Directory <<<<<<<<\e[0m"
 mkdir /app
+
+echo -e "\e[36m>>>>>>>>>> Download App Content <<<<<<<<\e[0m"
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>>> Unzip App content <<<<<<<<\e[0m"
 unzip /tmp/cart.zip
+
+echo -e "\e[36m>>>>>>>>>> install NodeJS Dependencies <<<<<<<<\e[0m"
 npm install
+
+echo -e "\e[36m>>>>>>>>>> Create Application Directory <<<<<<<<\e[0m"
 cp cart.service /etc/systemd/system/cart.service
+
+echo -e "\e[36m>>>>>>>>>> Start Cart Service  <<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable cart
 systemctl start cart
