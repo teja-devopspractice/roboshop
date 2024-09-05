4d5d6f7e1476cfd4abbbeaad50280a8dc4e374bc
teja-devopspractice <surlaraviteja60@gmail.com>
2024-09-05 18:25:47 +0530

    update

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
-echo -e "\e[36m>>>>>>>>>> Disable default mysql<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>> Disable default mysql <<<<<<<<\e[0m"
 dnf module disable mysql -y
 
-echo -e "\e[36m>>>>>>>>>> Copy Mysql.repo <<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>> Copy Mysql repo file <<<<<<<<\e[0m"
 cp /home/centos/roboshop/mysql.repo /etc/yum.repos.d/mysql.repo
 
 echo -e "\e[36m>>>>>>>>>> Install Mysql <<<<<<<<\e[0m"
@@ -9,8 +9,8 @@ dnf install mysql-community-server -y
 
 echo -e "\e[36m>>>>>>>>>> Start Mysql <<<<<<<<\e[0m"
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
 
-echo -e "\e[36m>>>>>>>>>> Set Password <<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>> Reset  Mysql Password <<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
 
