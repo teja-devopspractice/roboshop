f394a32240557b3d2ef89dde01ccd7b1fd9488e3
teja-devopspractice <surlaraviteja60@gmail.com>
2024-09-05 18:41:27 +0530

    update

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,10 @@
 yum install nginx -y
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp /home/centos/roboshop/roboshop.conf /etc/nginx/default.d/roboshop.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 systemctl enable nginx
-systemctl start nginx
+systemctl restart nginx
 
 
