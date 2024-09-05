aef790f4b1805ddac5060fbd58e7838f68e6f3be
teja-devopspractice <surlaraviteja60@gmail.com>
2024-09-05 15:37:25 +0530

    cart update

--- cart.sh
+++ cart.sh
@@ -21,7 +21,7 @@ echo -e "\e[36m>>>>>>>>>> install NodeJS Dependencies <<<<<<<<\e[0m"
 npm install
 
 echo -e "\e[36m>>>>>>>>>> Create Application Directory <<<<<<<<\e[0m"
-cp cart.service /etc/systemd/system/cart.service
+cp /home/centos/roboshop/cart.service /etc/systemd/system/cart.service
 
 echo -e "\e[36m>>>>>>>>>> Start Cart Service  <<<<<<<<\e[0m"
 systemctl daemon-reload
