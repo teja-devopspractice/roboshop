fe5d0d32f16404c0613a14792a6958fc562ca03e
teja-devopspractice <surlaraviteja60@gmail.com>
2024-09-05 12:06:30 +0530

    redis.sh

--- redis.sh
+++ redis.sh
@@ -4,7 +4,7 @@ echo -e "\e[35m>>>>>>>>>>Installing Redis<<<<<<<<\e[0m"
 dnf module enable redis:remi-6.2 -y
 dnf install redis -y
 echo -e "\e[35m>>>>>>>>>>Update listen address<<<<<<<<\e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.|' /etc/redis/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis/redis.conf
 echo -e "\e[35m>>>>>>>>>>Start Redis service<<<<<<<<\e[0m"
 systemctl enable redis
 systemctl start redis
